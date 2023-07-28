79829997fa9a0d0fb6aaf3d8213224222717178c
RadioFox <radiofoxinnovation@gmail.com>
2023-07-29 00:15:09 +0800

    roughly 1ms delay using 11.0592MHz crystal on STC89CRC52.

--- 000_delay/main.c
+++ 000_delay/main.c
@@ -1,14 +1,29 @@
 #include <stc89.h> 
 
 void delay_ms(unsigned int t){
-    unsigned char x;
+    unsigned int x;
     while(t--)
-        for(x=0;x<120;x--);
+        for(x=0;x<126;x++);
 }
+
+void delay(unsigned int t){
+    /* 
+        delay(1) produce 41.250us
+        delay(2) produce 53.167us
+        delay(3) produce 65.125us
+    */
+    while(t--);
+}
+
 int main(){
     while(1){
-        P2 = ~P2;
-        delay_ms(150);
+        /*  for bit toggle use !
+                P2_0 = !P2_0;
+            for byte toggle use ~
+                P2 = ~P2; 
+        */
+        P2_7 = !P2_7;
+        delay_ms(2);
     }
     return 0;
 }
